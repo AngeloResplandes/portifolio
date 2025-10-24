ea3ec6ba2918fe5416f3d9afb4e1239780228240
AngeloResplandes <angeloodrigues@gmail.com>
2025-10-23 21:26:32 -0300

    feat: create first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 const App = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <h1 className="font-bold text-4xl">Portifólio</h1>
-    </div>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="font-bold text-4xl my-3">Portifólio</h1>
+      <p>Name: Ângelo Resplandes</p>
+      <p>Version: v1</p>
+      <p>Status: operational</p>
+    </div >
   )
 }
 
