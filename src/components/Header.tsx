df9913f768d5b92d0106b0125ae8894f06a6059c
AngeloResplandes <angeloodrigues@gmail.com>
2025-10-29 14:01:33 -0300

    feat: main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,13 @@ const url = {
 
 export const Header = () => {
     return (
-        <nav className="fixed top-0 z-50 w-full flex justify-center gap-4">
+        <nav className="bg-[#1E1733] fixed top-0 z-50 w-full flex justify-center gap-4">
             <a
                 href={url.github}
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Abrir GitHub"
-                className="inline-flex items-center gap-2 rounded-lg mx-2 my-4 hover:underline"
+                className="inline-flex items-center gap-2 mx-2 my-4 hover:underline"
             >
                 <FaGithub className="h-5 w-5" />
                 <span>GitHub</span>
@@ -25,7 +25,7 @@ export const Header = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Abrir Instagram"
-                className="inline-flex items-center gap-2 rounded-lg mx-2 my-4 hover:underline"
+                className="inline-flex items-center gap-2 mx-2 my-4 hover:underline"
             >
                 <FaInstagram className="h-5 w-5" />
                 <span>Instagram</span>
@@ -36,7 +36,7 @@ export const Header = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Abrir LinkedIn"
-                className="inline-flex items-center gap-2 rounded-lg mx-2 my-4 hover:underline"
+                className="inline-flex items-center gap-2 mx-2 my-4 hover:underline"
             >
                 <FaLinkedin className="h-5 w-5" />
                 <span>LinkedIn</span>
