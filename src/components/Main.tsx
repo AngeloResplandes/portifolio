931e8282c756e47b034a38790940d405c27d2465
AngeloResplandes <angeloodrigues@gmail.com>
2025-10-28 23:54:46 -0300

    feat: main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,34 @@
+import { TiMessages } from "react-icons/ti";
+
 export const Main = () => {
     return (
         <div className="h-screen w-full flex justify-center items-center">
-            <h1>Angelo Resplandes</h1>
+            <div className="h-72 flex flex-col justify-evenly items-center text-center text-xl">
+                <div>
+                    <p className="mb-3">Olá! Sou o</p>
+                    <h1 className="text-5xl font-medium mb-5">
+                        <span className="animate-color-cycle">
+                            Angelo Resplandes
+                        </span>
+                    </h1>
+                </div>
+                <div className="mb-10">
+                    <p>Um Desenvolvedor de Software apaixonado por tecnologia.</p>
+                    <p>Meu propósito é criar soluções e inovar com novas ideias.</p>
+                </div>
+                <a
+                    href=""
+                    target=""
+                    rel="noopener noreferrer"
+                    aria-label="Abrir Formulário"
+                    className={`bg-white text-[#665D91] flex items-center 
+                        w-auto h-8 p-6 rounded-lg transition duration-300 
+                        ease-in-out hover:bg-[#665D91] hover:text-white
+                        hover:shadow-xl`}>
+                    <TiMessages className="h-5 w-5 mr-3" />
+                    <span className="font-medium">Vamos conversar!</span>
+                </a>
+            </div>
         </div>
     );
 }
